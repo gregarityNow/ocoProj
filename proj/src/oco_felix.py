95441646b80efae791d1ea18b1ecf685b3bf350c
felix <felix.herron@gmail.com>
2022-12-30 20:48:49 +0100

    oyyy

--- proj/src/oco_felix.py
+++ proj/src/oco_felix.py
@@ -607,7 +607,7 @@ def gradient_descent(data, opt, lrStrat = "epochPro", n_epochs = 100, batch_size
 
             allLossesFtWt.append(loss);
 
-            fullBatchLoss = getLoss(x_train[allIdx], y_train[allIdx], wFullBatch)
+            fullBatchLoss = getLoss(x_train[:,np.array(allIdx)], y_train[np.array(allIdx)], wFullBatch)
             epochRegret = np.sum(allLossesFtWt) - fullBatchLoss
             allRegrets.append(epochRegret)
 
