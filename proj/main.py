fd18aa9bdd9536e2c97919412dac3dc631d9d0a0
felix <felix.herron@gmail.com>
2022-12-31 02:06:50 +0100

    oyyy

--- proj/main.py
+++ proj/main.py
@@ -64,7 +64,6 @@ def mainHypSearch(descType, n_epochs=(10000 if opt.n_epochs == -1 else opt.n_epo
 					continue
 				else:
 					print("not seen",descType, batch_size, projDim, regLamb)
-					continue;
 
 				try:
 					gradient_descent(data, opt, lrStrat="epochPro", n_epochs=(n_epochs if batch_size != -1 else int(n_epochs/10)), batch_size=batch_size, regLamb=regLamb, fake=False,
